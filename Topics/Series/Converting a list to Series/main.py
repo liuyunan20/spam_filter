3ae98db260a6c71adf1e9e47da1c28d5c0608959
liuyunan <liuyunan.20@gmail.com>
2022-07-18 15:43:06 -0700

    better code style, delete the variable name that only for return

--- Topics/Series/Converting a list to Series/main.py
+++ Topics/Series/Converting a list to Series/main.py
@@ -2,5 +2,4 @@ import pandas as pd
 
 
 def create_series(foods, calories):
-    calorie_series = pd.Series(calories, index=foods, name='Calorie content')
-    return calorie_series
+    return pd.Series(calories, index=foods, name='Calorie content')
